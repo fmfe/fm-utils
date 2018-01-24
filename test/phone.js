c933b3a72a4cac1f45263634f69ef78104eb7ee6
modood <modood@qq.com>
2018-01-24 12:40:36 +0800

    refactor: 使用 awesome-phonenumber 替换 google-libphonenumber

--- test/phone.js
+++ test/phone.js
@@ -10,10 +10,17 @@ describe('lib: phone', () => {
             expect(phone.isValid(13800138000)).to.equal(true);
             expect(phone.isValid(8613800138000)).to.equal(true);
 
+            expect(phone.isValid(1273257554)).to.equal(false);
+            expect(phone.isValid(441273257554)).to.equal(false);
+
             done();
         });
         it('should return false if country code is invalid', done => {
             expect(phone.isValid(13800138000, 'invalid country code')).to.equal(false);
+
+            expect(phone.isValid(4322391750, {})).to.equal(false);
+            expect(phone.isValid(4322391750, [])).to.equal(false);
+            expect(phone.isValid(4322391750, true)).to.equal(false);
 
             done();
         });
@@ -21,18 +28,26 @@ describe('lib: phone', () => {
             // China
             expect(phone.isValid(13800138000, 86)).to.equal(true);
             expect(phone.isValid(8613800138000, 86)).to.equal(true);
+            expect(phone.isValid(13800138000, 'CN')).to.equal(true);
+            expect(phone.isValid(8613800138000, 'CN')).to.equal(true);
 
             // United Kingdom
             expect(phone.isValid(1273257554, 44)).to.equal(true);
             expect(phone.isValid(441273257554, 44)).to.equal(true);
+            expect(phone.isValid(1273257554, 'GB')).to.equal(true);
+            expect(phone.isValid(441273257554, 'GB')).to.equal(true);
 
             // United States
             expect(phone.isValid(4322391750, 1)).to.equal(true);
             expect(phone.isValid(14322391750, 1)).to.equal(true);
+            expect(phone.isValid(4322391750, 'US')).to.equal(true);
+            expect(phone.isValid(14322391750, 'US')).to.equal(true);
 
             // Canada
             expect(phone.isValid(8252504025, 1)).to.equal(true);
             expect(phone.isValid(18252504025, 1)).to.equal(true);
+            expect(phone.isValid(4322391750, 'CA')).to.equal(true);
+            expect(phone.isValid(14322391750, 'CA')).to.equal(true);
 
             done();
         });
@@ -41,22 +56,30 @@ describe('lib: phone', () => {
             // Number: China
             // Code: United Kingdom
             expect(phone.isValid(13800138000, 44)).to.equal(false);
-            expect(phone.isValid(8613800138000, 44)).to.equal(false);
+            expect(phone.isValid(13800138000, 44)).to.equal(false);
+            expect(phone.isValid(8613800138000, 'GB')).to.equal(false);
+            expect(phone.isValid(8613800138000, 'GB')).to.equal(false);
 
             // Number: United Kingdom
             // Code: United States or Canada ...
             expect(phone.isValid(1273257554, 1)).to.equal(false);
             expect(phone.isValid(441273257554, 1)).to.equal(false);
+            expect(phone.isValid(1273257554, 'US')).to.equal(false);
+            expect(phone.isValid(441273257554, 'US')).to.equal(false);
 
             // Number: United States
             // Code: China
             expect(phone.isValid(4322391750, 86)).to.equal(false);
             expect(phone.isValid(14322391750, 86)).to.equal(false);
+            expect(phone.isValid(4322391750, 'CN')).to.equal(false);
+            expect(phone.isValid(14322391750, 'CN')).to.equal(false);
 
             // Number: Canada
             // Code: China
             expect(phone.isValid(8252504025, 886)).to.equal(false);
             expect(phone.isValid(18252504025, 886)).to.equal(false);
+            expect(phone.isValid(8252504025, 'TW')).to.equal(false);
+            expect(phone.isValid(18252504025, 'TW')).to.equal(false);
 
             done();
         });
